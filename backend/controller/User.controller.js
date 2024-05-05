7995350560d4cc940385d0956fc6b5f830b814c4
Suhani Jain <jainsuhani132@gmail.com>
2024-05-06 01:26:42 +0530

    implementing login API

--- backend/controller/User.controller.js
+++ backend/controller/User.controller.js
@@ -6,7 +6,7 @@ export const signup= async (req,res)=>{
         const {fullname,email,password}=req.body;
         const user = await User.findOne({email})
         if(user){
-            return res.status(400).json({"message":"User already exists"});
+            return res.status(400).json({message:"User already exists"});
         }
         const hashPassword = await bcryptjs.hash(password,8);
         const createdUser = new User({
@@ -15,10 +15,30 @@ export const signup= async (req,res)=>{
             "password":hashPassword,
         })
         await createdUser.save()
-        return res.status(201).json({"message":"User created successfully"});
+        return res.status(201).json({message:"User created successfully"});
 
     }catch(error){
         console.log("error :" + error.message);
         return res.status(501).json({"message":"Internal server Error"});
+    }
+};
+export const login = async (req,res)=>{
+    try{
+        const{email,password}= req.body;
+        const user = await User.findOne({email})
+        const isMatch = await bcryptjs.compare(password,user.password)
+        if(!user || !isMatch){
+            return res.status(400).json({message:"Invalid useranme or password"});
+        }
+        else{
+           res.status(200).json({message:"User logged in successfully",user:{
+            _id:user._id,
+            fullname:user.fullname,
+            email:user.email
+           }});
+        }
+    }catch(error){
+        console.log("Error :"+error.message);
+            res.status(500).json({message:"Internal server error"})
     }
 }
--- backend/route/User.route.js
+++ backend/route/User.route.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import {signup} from "../controller/User.controller.js";
+import {login} from "../controller/User.controller.js";
 const router = express.Router()
 
 router.post("/signup",signup)
+router.post("/login",login)
 
 
 export default router;
